4247184841ef3b7c81a206569b7b94e82dd71ec1
Toreole <toreole.official@gmail.com>
2020-02-23 22:28:24 +0100

    Copy over contents from test project

---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController cc;
+    public PlayerInput inputComponent;
+    public float speed;
+    public float sprintSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //inputComponent.onActionTriggered += HandleInput;
+        var pad = Gamepad.current;
+        if(pad != null)
+        {
+            Vector2 input = pad.leftStick.ReadValue();
+            Vector3 move = new Vector3(input.x, 0, input.y);
+            move *= pad.rightShoulder.isPressed? sprintSpeed : speed;
+            cc.SimpleMove(move);
+        }
+    }
+
+    void HandleInput(InputAction.CallbackContext context)
+    {
+        //thonk
+    }
+}
