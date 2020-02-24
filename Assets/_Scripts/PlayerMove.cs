09e164c07b1e319c7116fdcf4b2bf7c3e2522f7d
Toreole <toreole.official@gmail.com>
2020-02-24 12:27:48 +0100

    Getting to know the InputSystem more

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -10,26 +10,32 @@ public class PlayerMove : MonoBehaviour
     public float speed;
     public float sprintSpeed;
 
+    private InputAction moveAction;
+
     private void Start()
     {
-        
+        moveAction = inputComponent.currentActionMap.FindAction("Move");
     }
 
     void Update()
     {
-        //inputComponent.onActionTriggered += HandleInput;
+        //print(inputComponent.currentControlScheme); //Keyboard&Mouse || Gamepad
+        
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move *= speed;
+        cc.SimpleMove(move);
+    }
+
+    void GamepadMoveDirect()
+    {
         var pad = Gamepad.current;
-        if(pad != null)
+        if (pad != null)
         {
             Vector2 input = pad.leftStick.ReadValue();
             Vector3 move = new Vector3(input.x, 0, input.y);
-            move *= pad.rightShoulder.isPressed? sprintSpeed : speed;
+            move *= pad.rightShoulder.isPressed ? sprintSpeed : speed;
             cc.SimpleMove(move);
         }
-    }
-
-    void HandleInput(InputAction.CallbackContext context)
-    {
-        //thonk
     }
 }
