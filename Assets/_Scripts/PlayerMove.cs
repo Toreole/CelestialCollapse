fb1bb1f0ae3a8380164357c8bece3995dd88fc43
Toreole <toreole.official@gmail.com>
2020-02-25 18:45:06 +0100

    Import input rebinding demo

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XInput;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -15,13 +16,39 @@ public class PlayerMove : MonoBehaviour
     private void Start()
     {
         moveAction = inputComponent.currentActionMap.FindAction("Move");
+        foreach (InputAction action in inputComponent.currentActionMap.actions)
+        {
+            print($"----{action.name}----");
+            //print(action.GetBindingDisplayString());
+            //for (int i = 0; i < action.bindings.Count; i++)
+            //    print(action.GetBindingDisplayString(i));
+            
+            int index = action.GetBindingIndex(InputBinding.MaskByGroups("Keyboard", "Mouse"));
+            if(index >= 0)
+                print(action.GetBindingDisplayString(index));
+            
+            //foreach (InputBinding bind in action.bindings)
+            //{
+            //    if(bind.effectivePath.Contains("<Keyboard>") || bind.effectivePath.Contains("<Mouse>"))
+            //        print($"{action.name} bound to {bind.path}");
+            //}
+        }
+        //TODO: okay cool this actually works wow.
+        //yield return new WaitForSeconds(1f);
+        //var controller = Gamepad.current;
+        //if (controller != null)
+        //{
+        //    controller.SetMotorSpeeds(3f, 3f); //left and right,
+        //    yield return new WaitForSeconds(0.5f);
+        //    controller.SetMotorSpeeds(0.0f, 0.0f);
+        //}
     }
 
     void Update()
     {
         //print(inputComponent.currentControlScheme); //Keyboard&Mouse || Gamepad
-        
-        Vector2 input = moveAction.ReadValue<Vector2>();
+
+           Vector2 input = moveAction.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
         move *= speed;
         cc.SimpleMove(move);
