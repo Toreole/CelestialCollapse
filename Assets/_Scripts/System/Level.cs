3b087e4aafb97fc2ea8dc55b029478b97dd8fb90
Toreole <toreole.official@gmail.com>
2020-04-08 18:45:25 +0200

    Adding Naughty Attributes

---
+++ Assets/_Scripts/System/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Celestial
+{
+    public class Level : MonoBehaviour
+    {
+        //static shiz
+        public static Level current { get; private set; }
+
+        public static event System.Action<Level> OnModifierChanged;
+        
+        //private runtime shit
+        private List<LevelModifier> activeModifiers = new List<LevelModifier>();
+        private float enemyHealthMod = 1.0f;
+
+        //public props.
+        public float EnemyHealthMod => enemyHealthMod;
+
+        public void AddModifier(LevelModifier mod)
+        {
+            activeModifiers.Add(mod);
+            enemyHealthMod *= mod.enemyHealth; //multiplicative.
+        }
+
+    }
+}
---
+++ Assets/_Scripts/System/LevelModifier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Celestial
+{
+    [CreateAssetMenu(fileName = "new Level Modifier", menuName = "SO/Level/Modifier")]
+    public class LevelModifier : ScriptableObject
+    {
+        [Min(1)]
+        public float enemyHealth;
+    }
+}
